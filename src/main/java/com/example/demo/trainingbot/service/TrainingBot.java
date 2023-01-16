cbe3805d84804402800bdc36d3df28d5438f6bf1
Vitaliy Vivian <v.vivian@a-academy.by>
2023-01-16 19:43:26 +0100

    Add markup menu

--- src/main/java/com/example/demo/trainingbot/service/TrainingBot.java
+++ src/main/java/com/example/demo/trainingbot/service/TrainingBot.java
@@ -4,9 +4,15 @@ import com.example.demo.trainingbot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -15,6 +21,17 @@ public class TrainingBot extends TelegramLongPollingBot {
 
     public TrainingBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start","command to start talk"));
+        listOfCommands.add(new BotCommand("/help","command to call help"));
+        listOfCommands.add(new BotCommand("/mydata","command to show my data"));
+        listOfCommands.add(new BotCommand("/deletedata","command to delete my data"));
+        listOfCommands.add(new BotCommand("/settings","command to change settings of the bot"));
+        try {
+            this.execute( new SetMyCommands(listOfCommands, new BotCommandScopeDefault(),null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list" + e.getMessage());
+        }
     }
 
     @Override
